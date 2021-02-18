c855c2a4174874b28bde537e563c02a84b0ddb34
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-02-18 13:09:26 -0700

    question and 2 strings

---
+++ main.js
@@ -0,0 +1,18 @@
+function task(str){
+    return str + "???";
+}
+
+const result1 = task("Hello");
+result1;
+
+const result2 = task ('what even');
+result2;
+
+function task(str, str1){
+    return str + " " + str1;
+}
+
+const task1 = task('oh', 'hi')
+task1;
+const task2 = task('well', 'hello back')
+task2;
